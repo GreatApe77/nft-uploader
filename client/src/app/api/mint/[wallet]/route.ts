0f1876141f8d8e31ef6760b24780afc86fb46d00
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-10-10 10:12:16 -0300

    cria rota proxy para o backend no client

---
+++ client/src/app/api/mint/[wallet]/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request, context: any) {
+	const wallet = context.params.wallet;
+	const formData = await request.formData();
+    let endpoint:string
+    if(process.env.NODE_ENV==="development"){
+        endpoint = `${process.env.DEV_BACKEND_ENDPOINT}`
+    }else if (process.env.NODE_ENV==="production"){
+        endpoint = `${process.env.PROD_BACKEND_ENDPOINT}`
+    }
+	const res = await fetch(`${endpoint!}/mint/${wallet}`, {
+		method: "POST",
+		body: formData,
+	});
+	const jsonRes = await res.json();
+	return NextResponse.json({
+		mintedTo: wallet,
+		status: res.status,
+		data: jsonRes,
+	});
+	
+}
