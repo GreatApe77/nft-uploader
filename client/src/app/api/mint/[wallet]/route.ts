18b18e5d0d28b2f68ff4176e09b48790fc468577
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-10-15 10:30:53 -0300

    tentando resolver problema

--- client/src/app/api/mint/[wallet]/route.ts
+++ client/src/app/api/mint/[wallet]/route.ts
@@ -1,5 +1,6 @@
-import axios,{AxiosRequestConfig} from "axios";
+
 import { NextResponse } from "next/server";
+import fetch from "node-fetch";
 
 export async function POST(request: Request, context: any) {
 	const wallet = context.params.wallet;
@@ -15,16 +16,25 @@ export async function POST(request: Request, context: any) {
     const formData = await request.formData()
     console.log(formData)
     try {
-        const axiosConfig: AxiosRequestConfig = {
-            headers: Object.fromEntries(request.headers), // Convert Headers to an object
-        };
+       // const axiosConfig: AxiosRequestConfig = {
+        //   headers: Object.fromEntries(request.headers), 
+         //   httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+//
+        //};
 
         //`${endpoint!}/mint/${wallet}`
-        const axiosResponse = await axios.post(`${endpoint!}/mint/${wallet}`,formData,axiosConfig);
+        //const axiosResponse = await axios.post(`${endpoint!}/mint/${wallet}`,formData,axiosConfig);
+        //request.headers.set("Content-Length", formData.length);
 
+        const res = await fetch(`${endpoint!}/mint/${wallet}`,{
+            method:"POST",
+            headers:request.headers,
+            body:request.body as any
+        })
+        const jsonRes = await res.json()
         return NextResponse.json({
-            status: axiosResponse.status,
-            data: axiosResponse.data,
+            status: res.status,
+            data: jsonRes,
         });
     } catch (error) {
         console.error(error)
