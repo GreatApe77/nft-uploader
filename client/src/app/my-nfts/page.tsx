27f69fed80d807eb829eff2211b2fa84b3bf7d17
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-10-18 21:49:58 -0300

    pronto para producao mas ainda incompleto

--- client/src/app/my-nfts/page.tsx
+++ client/src/app/my-nfts/page.tsx
@@ -3,6 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css"
 export default function viewNFTs() {
   return (
     <div>
+      <h1 className="display-1">Seus NFTS aparecerão Aqui</h1>
+      <h2 className="display-3">Ainda em Desenvolvimento :)</h2>
       <NFTAlbum/>
     </div>
   )
